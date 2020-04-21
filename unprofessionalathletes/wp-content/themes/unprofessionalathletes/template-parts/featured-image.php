7d3b7b51ccf6051cc0c8eb7471c7ada98393ddba
robbbino <robin.olsson@live.co.uk>
2020-04-21 12:23:00 +0100

    New Archive Template & Styling

--- unprofessionalathletes/wp-content/themes/unprofessionalathletes/template-parts/featured-image.php
+++ unprofessionalathletes/wp-content/themes/unprofessionalathletes/template-parts/featured-image.php
@@ -7,19 +7,23 @@
  * @since 1.0.0
  */
 
-if ( has_post_thumbnail() && ! post_password_required() ) {
+
+// Remove deault If statement (always pull in featured media even if blank - it is replaced with colour in style.css).
+// if ( has_post_thumbnail() && ! post_password_required() ) {
 
 	$featured_media_inner_classes = '';
 
-	// Make the featured media thinner on archive pages.
+	// Make the featured media thinner on archive pages, plus give a random background colour that will be displayed if no image is selected.
 	if ( ! is_singular() ) {
+		$background_colors = array('#65A9AC', '#005500', '#E9DB00', '#FF4500', '#9AB19A');
+		$rand_background = $background_colors[array_rand($background_colors)];
 		$featured_media_inner_classes .= ' medium';
 	}
 	?>
 
 	<figure class="featured-media">
 
-		<div class="featured-media-inner section-inner<?php echo $featured_media_inner_classes; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>">
+		<div class="featured-media-inner section-inner<?php echo $featured_media_inner_classes; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?>" style="background-color:<?php echo $rand_background; ?>">
 
 			<?php
 			the_post_thumbnail();
@@ -40,4 +44,5 @@ if ( has_post_thumbnail() && ! post_password_required() ) {
 	</figure><!-- .featured-media -->
 
 	<?php
-}
+//}
+
