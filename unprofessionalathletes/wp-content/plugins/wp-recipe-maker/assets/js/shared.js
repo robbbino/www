f8d92271cd97966ca3cb98d73f1a5dc821233e0e
robbbino <robin.olsson@live.co.uk>
2020-04-22 13:22:15 +0100

    Removed WP Recipe Maker Pliugin

--- unprofessionalathletes/wp-content/plugins/wp-recipe-maker/assets/js/shared.js
+++
@@ -1,9 +0,0 @@
-// Polyfills.
-if (!global._babelPolyfill) { require('babel-polyfill'); }
-import 'whatwg-fetch';
-
-// Global variables.
-import { createHooks } from '@wordpress/hooks';
-let hooks = createHooks();
-
-export { hooks };
--- unprofessionalathletes/wp-content/plugins/wp-recipe-maker/assets/js/shared/Browser.js
+++
@@ -1,9 +0,0 @@
-import Bowser from 'bowser';
-
-export function isProblemBrowser() {
-    const browser = Bowser.getParser( window.navigator.userAgent );
-    return browser.satisfies({
-        edge: "<80",
-        ie: '>0',
-    });
-}
